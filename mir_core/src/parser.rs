c712df16cd89c90520710592808f9167fa91ce6e
Paul Russo <paul@paulrusso.me>
2024-09-29 00:27:30 -0600

    Remove cached thing

--- mir_core/src/parser.rs
+++ mir_core/src/parser.rs
@@ -1,42 +1,9 @@
 pub use pest::Token;
 use pest::{iterators::Pairs, Parser};
-use std::collections::HashMap;
-use string_interner::{DefaultBackend, DefaultSymbol, StringInterner};
 
 #[derive(Parser)]
 #[grammar = "math.pest"]
 pub struct MathParser;
-
-pub struct CachedParser<'a> {
-    interner: StringInterner<DefaultBackend>,
-    cache: HashMap<DefaultSymbol, Result<Pairs<'a, Rule>, pest::error::Error<Rule>>>,
-}
-
-impl<'a> CachedParser<'a> {
-    pub fn new() -> CachedParser<'a> {
-        CachedParser {
-            interner: StringInterner::default(),
-            cache: HashMap::new(),
-        }
-    }
-
-    pub fn get_pairs(
-        &'a mut self,
-        input: String,
-    ) -> Result<Pairs<'a, Rule>, pest::error::Error<Rule>> {
-        let symbol = self.interner.get_or_intern(input);
-
-        match self.cache.get(&symbol) {
-            Some(pairs) => pairs.clone(),
-            None => {
-                let interned_input = self.interner.resolve(symbol).unwrap();
-                let result = MathParser::parse(Rule::input, &interned_input);
-                self.cache.insert(symbol, result.clone());
-                result
-            }
-        }
-    }
-}
 
 pub fn get_pairs(input: &String) -> Result<Pairs<Rule>, pest::error::Error<Rule>> {
     MathParser::parse(Rule::input, input)
