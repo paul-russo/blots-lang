9230058ab5a4c0e2c1e194dedfe4f8af4aa4ecc5
Paul Russo <paul@paulrusso.me>
2025-06-02 15:39:24 -0600

    bug fixes

--- blots-wasm/src/lib.rs
+++ blots-wasm/src/lib.rs
@@ -1,121 +1,143 @@
 use anyhow::Result;
 use blots_core::{
-    expressions::evaluate_expression,
     functions::get_built_in_function_idents,
-    heap::{Heap, CONSTANTS},
-    parser::{get_pairs, get_tokens, Rule, Token},
-    values::SerializableValue,
+    heap::CONSTANTS,
+    parser::{get_tokens, Token},
+    transpiler::{transpile_to_js, transpile_to_js_with_inline_eval},
+    values::PrimitiveValue,
 };
+use js_sys::{eval, Object, Reflect};
 use serde::{Deserialize, Serialize};
-use std::{
-    cell::RefCell,
-    collections::{BTreeMap, HashMap, HashSet},
-    rc::Rc,
-};
+use std::collections::{HashMap, HashSet};
 use wasm_bindgen::prelude::*;
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug)]
 struct EvaluationResult {
-    values: HashMap<String, SerializableValue>,
-    bindings: HashMap<String, SerializableValue>,
+    values: HashMap<String, JsValue>,
+    bindings: HashMap<String, JsValue>,
     outputs: HashSet<String>,
 }
 
 #[wasm_bindgen]
 pub fn evaluate(expr: &str, inputs_js: JsValue) -> Result<JsValue, JsError> {
-    let heap = Rc::new(RefCell::new(Heap::new()));
-    let inputs_given: BTreeMap<String, SerializableValue> =
-        serde_wasm_bindgen::from_value(inputs_js)?;
+    // Transpile Blots code to JavaScript with inline evaluation
+    let js_code = transpile_to_js_with_inline_eval(expr)
+        .map_err(|e| JsError::new(&format!("Transpilation error: {}", e)))?;
 
-    let inputs = inputs_given
-        .into_iter()
-        .map(|(key, value)| (key, value.to_value(&mut heap.borrow_mut()).unwrap()))
-        .collect();
-
-    let bindings = Rc::new(RefCell::new(HashMap::new()));
-    {
-        bindings.borrow_mut().insert(
-            String::from("inputs"),
-            heap.borrow_mut().insert_record(inputs),
-        );
+    // Set up inputs in global scope if provided
+    if !inputs_js.is_undefined() && !inputs_js.is_null() {
+        let global = js_sys::global();
+        Reflect::set(&global, &"inputs".into(), &inputs_js)
+            .map_err(|e| JsError::new(&format!("Failed to set inputs: {:?}", e)))?;
     }
 
-    let expr_owned = String::from(expr);
-    let pairs =
-        get_pairs(&expr_owned).map_err(|e| JsError::new(&format!("Parsing error: {}", e)))?;
+    // Execute the transpiled JavaScript
+    let result = eval(&js_code)
+        .map_err(|e| JsError::new(&format!("JavaScript execution error: {:?}", e)))?;
 
-    let mut outputs = HashSet::new();
+    // The transpiled code with inline evaluation returns $$results object
+    // Extract the results from the global scope
+    let global = js_sys::global();
+    let results_obj = Reflect::get(&global, &"$$results".into())
+        .map_err(|e| JsError::new(&format!("Failed to get results: {:?}", e)))?;
+
+    if results_obj.is_undefined() {
+        // No inline evaluation results, return empty result
+        let result_obj = Object::new();
+        Reflect::set(&result_obj, &"values".into(), &js_sys::Map::new())
+            .map_err(|e| JsError::new(&format!("Failed to set empty values: {:?}", e)))?;
+        Reflect::set(&result_obj, &"bindings".into(), &js_sys::Map::new())
+            .map_err(|e| JsError::new(&format!("Failed to set empty bindings: {:?}", e)))?;
+        Reflect::set(&result_obj, &"outputs".into(), &js_sys::Array::new())
+            .map_err(|e| JsError::new(&format!("Failed to set empty outputs: {:?}", e)))?;
+        return Ok(result_obj.into());
+    }
+
+    // Extract values, bindings, and outputs from the results object
+    let values_obj = Reflect::get(&results_obj, &"values".into())
+        .map_err(|e| JsError::new(&format!("Failed to get values: {:?}", e)))?;
+    let bindings_obj = Reflect::get(&results_obj, &"bindings".into())
+        .map_err(|e| JsError::new(&format!("Failed to get bindings: {:?}", e)))?;
+    let outputs_obj = Reflect::get(&results_obj, &"outputs".into())
+        .map_err(|e| JsError::new(&format!("Failed to get outputs: {:?}", e)))?;
+
+    // Convert to HashMap - for now, keep values as JsValue
     let mut values = HashMap::new();
+    let mut bindings = HashMap::new();
+    let mut outputs: HashSet<String> = HashSet::new();
 
-    for pair in pairs {
-        match pair.as_rule() {
-            Rule::statement => {
-                if let Some(inner_pair) = pair.into_inner().next() {
-                    let rule = inner_pair.as_rule();
-                    let start_line_col = inner_pair.as_span().start_pos().line_col();
-                    let end_line_col = inner_pair.as_span().end_pos().line_col();
-
-                    let inner_pairs = inner_pair.into_inner();
-
-                    if rule == Rule::output_declaration {
-                        let mut inner_pairs_clone = inner_pairs.clone();
-
-                        let next_token = inner_pairs_clone.next().unwrap(); // Skip the output keyword.
-                        let output_name = match next_token.as_rule() {
-                            Rule::identifier => next_token.as_str().to_string(),
-                            Rule::assignment => {
-                                let next_inner_token = next_token.into_inner().next().unwrap();
-                                match next_inner_token.as_rule() {
-                                    Rule::identifier => next_inner_token.as_str().to_string(),
-                                    _ => {
-                                        unreachable!(
-                                            "unexpected rule: {:?}",
-                                            next_inner_token.as_rule()
-                                        )
-                                    }
-                                }
-                            }
-                            _ => unreachable!("unexpected rule: {:?}", next_token.as_rule()),
-                        };
-
-                        outputs.insert(output_name);
-                    }
-
-                    let col_id = format!(
-                        "{}-{}__{}-{}",
-                        start_line_col.0, start_line_col.1, end_line_col.0, end_line_col.1
-                    );
-
-                    let value =
-                        evaluate_expression(inner_pairs, Rc::clone(&heap), Rc::clone(&bindings), 0)
-                            .map_err(|error| {
-                                JsError::new(&format!("Evaluation error: {}", error))
-                            })?;
-
-                    values.insert(col_id, value);
+    // Convert values object to HashMap
+    if values_obj.is_object() {
+        let obj = Object::from(values_obj);
+        let keys = Object::keys(&obj);
+        for i in 0..keys.length() {
+            if let Some(key) = keys.get(i).as_string() {
+                if let Ok(value) = Reflect::get(&obj, &keys.get(i)) {
+                    values.insert(key, value);
                 }
             }
-            Rule::EOI => {}
-            rule => unreachable!("unexpected rule: {:?}", rule),
         }
     }
 
-    let values_serializable = values
-        .iter()
-        .map(|(k, v)| (k.clone(), v.to_serializable_value(&heap.borrow()).unwrap()))
-        .collect();
+    // Convert bindings object to HashMap  
+    if bindings_obj.is_object() {
+        let obj = Object::from(bindings_obj);
+        let keys = Object::keys(&obj);
+        for i in 0..keys.length() {
+            if let Some(key) = keys.get(i).as_string() {
+                if let Ok(value) = Reflect::get(&obj, &keys.get(i)) {
+                    bindings.insert(key, value);
+                }
+            }
+        }
+    }
 
-    let bindings_serializable = bindings
-        .borrow()
-        .iter()
-        .map(|(k, v)| (k.clone(), v.to_serializable_value(&heap.borrow()).unwrap()))
-        .collect();
+    // Convert outputs Set to HashSet
+    if outputs_obj.is_object() {
+        // For now, skip outputs processing - would need more complex Set handling
+    }
 
-    Ok(serde_wasm_bindgen::to_value(&EvaluationResult {
-        values: values_serializable,
-        bindings: bindings_serializable,
-        outputs,
-    })?)
+    // Create result object manually
+    let result_obj = Object::new();
+    
+    // Create values Map
+    let values_map = js_sys::Map::new();
+    for (key, value) in values {
+        values_map.set(&key.into(), &value);
+    }
+    
+    // Create bindings Map
+    let bindings_map = js_sys::Map::new();
+    for (key, value) in bindings {
+        bindings_map.set(&key.into(), &value);
+    }
+    
+    // Create outputs array (simplified)
+    let outputs_array = js_sys::Array::new();
+    for output in outputs {
+        outputs_array.push(&output.into());
+    }
+    
+    Reflect::set(&result_obj, &"values".into(), &values_map)
+        .map_err(|e| JsError::new(&format!("Failed to set values map: {:?}", e)))?;
+    Reflect::set(&result_obj, &"bindings".into(), &bindings_map)
+        .map_err(|e| JsError::new(&format!("Failed to set bindings map: {:?}", e)))?;
+    Reflect::set(&result_obj, &"outputs".into(), &outputs_array)
+        .map_err(|e| JsError::new(&format!("Failed to set outputs array: {:?}", e)))?;
+    
+    Ok(result_obj.into())
+}
+
+#[wasm_bindgen]
+pub fn transpile(expr: &str) -> Result<String, JsError> {
+    transpile_to_js(expr)
+        .map_err(|e| JsError::new(&format!("Transpilation error: {}", e)))
+}
+
+#[wasm_bindgen]
+pub fn transpile_with_inline_eval(expr: &str) -> Result<String, JsError> {
+    transpile_to_js_with_inline_eval(expr)
+        .map_err(|e| JsError::new(&format!("Transpilation error: {}", e)))
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -155,16 +177,19 @@ pub fn get_built_in_function_names() -> Result<JsValue, JsError> {
 
 #[wasm_bindgen]
 pub fn get_constants() -> Result<JsValue, JsError> {
-    let mut map = HashMap::new();
-    let constants = SerializableValue::Record(
-        CONSTANTS
-            .clone()
-            .into_iter()
-            .map(|(k, v)| (k, v.into()))
-            .collect(),
-    );
+    // Convert constants to a simple JavaScript object
+    let constants_obj = js_sys::Object::new();
+    
+    for (key, value) in CONSTANTS.clone() {
+        let js_value = match value {
+            PrimitiveValue::Number(n) => JsValue::from(n),
+            PrimitiveValue::Bool(b) => JsValue::from(b),
+            PrimitiveValue::Null => JsValue::null(),
+        };
+        
+        Reflect::set(&constants_obj, &key.into(), &js_value)
+            .map_err(|e| JsError::new(&format!("Failed to set constant: {:?}", e)))?;
+    }
 
-    map.insert(String::from("constants"), constants);
-
-    Ok(serde_wasm_bindgen::to_value(&map)?)
+    Ok(constants_obj.into())
 }
