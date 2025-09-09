73c13e01e13364bf05d47d96db5f5877649580df
Paul Russo <paul@paulrusso.me>
2025-09-09 15:12:36 -0600

    Add --inputs flag

--- blots/src/cli.rs
+++ blots/src/cli.rs
@@ -9,4 +9,8 @@ pub struct Args {
     /// Evaluate stdin as Blots source code instead of JSON inputs
     #[arg(short, long)]
     pub evaluate: bool,
+
+    /// JSON object to use as inputs (can be combined with piped inputs)
+    #[arg(short, long)]
+    pub inputs: Option<String>,
 }
