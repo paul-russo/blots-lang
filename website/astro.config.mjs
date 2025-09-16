ff97af3663140fb9b67ce7779b5053de016f2958
Paul Russo <paul@paulrusso.me>
2025-09-16 01:17:57 -0600

    add landing page

---
+++ website/astro.config.mjs
@@ -0,0 +1,19 @@
+// @ts-check
+import { defineConfig, fontProviders } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+
+// https://astro.build/config
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  experimental: {
+    fonts: [
+      {
+        provider: fontProviders.google(),
+        name: "Neuton",
+        cssVariable: "--font-neuton",
+      },
+    ],
+  },
+});
